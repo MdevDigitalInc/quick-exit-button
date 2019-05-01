6e13f6da3a3d91535cae1a5b2d2018177fdf652a
Nelson <nelson@mdev.digital>
2019-05-01 15:09:36 -0400

    Post-review changes

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-function exit() {
+(function() {
+  function exit() {
     window.location.replace('http://www.weathernetwork.ca');
-    // window.open("http://www.google.ca", "_newtab");
-}
-
-function eventListeners() {
-    document.getElementById("exit-button").addEventListener("click", exit);
-}
-
-document.addEventListener("DOMContentLoaded", function() { 
+  };
+  
+  function eventListeners() {
+    document.querySelector("[data-exit-button]").addEventListener("click", exit);
+  };
+  
+  document.addEventListener("DOMContentLoaded", function() { 
     eventListeners();
-});
+  });
+})();
