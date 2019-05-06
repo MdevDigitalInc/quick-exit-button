258616ac47234eb48c0c02ef90a3f7ac57eac0e0
Nelson <nelson@mdev.digital>
2019-05-06 14:41:05 -0400

    Upgraded script

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
 (function() {
+  window.onload = init();
+
+  function init() {
+    document.getElementById("menu").innerHTML = '<ul class="main-nav-list">'+
+    '<li><a href="index.html">Link A</a></li>'+
+    '<li><a href="page-2.html">Link B</a></li>'+
+    '<li><a href="page-3.html">Link C</a></li>'+
+    '<li><a href="page-4.html">Link D</a></li>'+ '</ul>';
+
+    var chk = document.getElementById("button");
+    if (chk.hidden == true) {
+      console.log("EXIT button is NOT ACTIVE!");
+    } else {
+      console.log("EXIT button is ACTIVE!");
+    }
+  }
+
   function exit() {
     window.location.replace('http://www.weathernetwork.ca');
   };
-  
-  function eventListeners() {
-    document.querySelector("[data-exit-button]").addEventListener("click", exit);
-  };
-  
-  document.addEventListener("DOMContentLoaded", function() { 
-    eventListeners();
+
+  var chk = document.getElementById("button");
+  document.querySelector("[data-exit-button]").addEventListener("click", exit);
+
+  document.addEventListener('keydown', function(e) {
+    console.log(e);
+    console.log(e.target.nodeName);
+    if (chk.hidden == true && e.which == 81) {
+      console.log("'Q' shortcut blocked! No EXIT button on this page.")
+    } else if (chk.hidden == false && e.which == 81 && e.target.nodeName == "BODY") {
+      exit();
+    }
   });
 })();
